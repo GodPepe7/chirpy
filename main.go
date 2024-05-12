3d5f0690e6f8f882d85e4bceb0753f5a089b876d
GodPepe7 <privatalexm@gmail.com>
2024-05-12 21:13:45 +0200

    add metrics routes

--- main.go
+++ main.go
@@ -11,9 +11,21 @@ func main() {
 
 	serveMux := http.NewServeMux()
 	fsHandler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
+	apiConfig := apiConfig{FileserverHits: 0}
+
 	serveMux.Handle("/app/*", apiConfig.middlewareMetricsInc(fsHandler))
+	serveMux.HandleFunc("GET /api/healthz", healthzHandler)
+	serveMux.HandleFunc("GET /api/reset", apiConfig.resetHandler)
+	serveMux.HandleFunc("GET /admin/metrics", apiConfig.metricsHandler)
+
 	server := &http.Server{Addr: ":" + port, Handler: serveMux}
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func healthzHandler(rw http.ResponseWriter, req *http.Request) {
+	rw.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	rw.WriteHeader(200)
+	rw.Write([]byte(http.StatusText(http.StatusOK)))
 }
--- middleware.go
+++ middleware.go
@@ -15,3 +15,21 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 		next.ServeHTTP(rw, req)
 	})
 }
+
+var tmpl = template.Must(template.ParseFiles("metrics.html"))
+
+func (cfg *apiConfig) metricsHandler(rw http.ResponseWriter, req *http.Request) {
+	rw.Header().Set("Content-Type", "text/html")
+	rw.WriteHeader(200)
+	err := tmpl.Execute(rw, cfg)
+	if err != nil {
+		rw.WriteHeader(500)
+	}
+}
+
+func (cfg *apiConfig) resetHandler(rw http.ResponseWriter, req *http.Request) {
+	cfg.FileserverHits = 0
+	rw.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	rw.WriteHeader(200)
+	rw.Write([]byte(http.StatusText(http.StatusOK)))
+}
